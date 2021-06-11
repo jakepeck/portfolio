e54dad6ea8cc472efc58c870e3e008f5b4ba7448
Jake Peck <jakepeck44@gmail.com>
2021-06-11 13:12:33 -0500

    fixing about page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,18 +3,11 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      Contact Page
-      <div>
+      <h1>Contact</h1>
+      <div className="contactDetails">
         <p>jakepeck44@gmail.com</p>
         <p>734-277-5410</p>
-        <p>
-          <a
-            target="#"
-            href="https://docs.google.com/document/d/1yfFNl3ThyxlHvLQDL653anpUWxS7WxuDTQieI5tmjq8"
-          >
-            Resume
-          </a>
-        </p>
+
         <p>
           <a target="#" href="https://linkedin.com/in/jake-peck">
             LinkedIn
@@ -28,6 +21,14 @@ const Contact = () => {
         <p>
           <a target="#" href="https://twitter.com/realjakepeck">
             Twitter
+          </a>
+        </p>
+        <p>
+          <a
+            target="#"
+            href="https://docs.google.com/document/d/1yfFNl3ThyxlHvLQDL653anpUWxS7WxuDTQieI5tmjq8"
+          >
+            Resume
           </a>
         </p>
       </div>
