6181f62fbc4d8172ecd4b59f5d8c4a33161ef658
Jake Peck <jakepeck44@gmail.com>
2021-06-11 12:04:18 -0500

    updated project card and projects pages

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const ProjectCard = () => {
-  return <div>Project Card</div>
+const ProjectCard = (props) => {
+  let { name, projectdesc, projecturl, projectimg } = props
+  return (
+    <div className="projectCard">
+      <img src={`${projectimg}`} alt="projectImage" width="200" /> <br />
+      <br />
+      <a target="#" href={`${projecturl}`}>
+        {name}
+      </a>{' '}
+      <br />
+      <p>{projectdesc}</p>
+    </div>
+  )
 }
 
 export default ProjectCard
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
-  return <div>Projects</div>
+  const myProjects = [
+    {
+      name: 'Happy Hour App',
+      projecturl: 'https://whispering-oasis-08122.herokuapp.com/',
+      projectimg: 'https://i.imgur.com/T0EIHWn.png',
+      projectdesc:
+        'Full-stack MERN (Mongoose, Express, React, Node) application that allows users to post, retrieve, update, and delete information on happy hours around the world.'
+    },
+    {
+      name: 'Go-Fish',
+      projecturl: 'https://jakepeck-go-fish.surge.sh',
+      projectimg: 'https://i.imgur.com/I3IZyR9.png',
+      projectdesc: 'JavaScript implementation of Go-Fish'
+    },
+    {
+      name: 'Tic-Tac-Toe',
+      projecturl: 'https://jakepeck-tic-tac-toe.surge.sh',
+      projectimg: 'https://i.imgur.com/bJMqh7e.png',
+      projectdesc: 'JavaScript implementation of Tic-Tac-Toe'
+    }
+  ]
+
+  let projectsDisplayed = myProjects.map((project, idx) => (
+    <ProjectCard
+      key={idx}
+      name={project.name}
+      projecturl={project.projecturl}
+      projectimg={project.projectimg}
+      projectdesc={project.projectdesc}
+    />
+  ))
+  return (
+    <div>
+      Projects
+      {projectsDisplayed}
+    </div>
+  )
 }
 
 export default Projects
